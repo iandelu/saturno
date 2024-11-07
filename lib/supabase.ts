3106c86506173d7792043ca4f30aebb676a2028a
Anedell <48674592+iandelu@users.noreply.github.com>
2024-11-07 22:33:09 +0100

    ANON

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,6 +2,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ANON_KEY as string
+const supabaseServiceKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
 export const supabase = createClient(supabaseUrl, supabaseServiceKey)
